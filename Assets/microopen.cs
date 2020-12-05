970f1e62a6b76703ddbfc3582501c5c1fed1412e
livypad <ishonghua.zhu@outlook.com>
2020-12-05 20:33:31 +0800

    麦克风代码
    每10秒写好一个wav文件保存在一个默认路径。需要在物体上面挂载microopen脚本，调用MicroPhoneInput执行

---
+++ Assets/microopen.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class microopen : MonoBehaviour {
+
+	// Use this for initialization
+	void Start() {
+	   StartCoroutine(DoCheck());
+	}
+
+    void OnGUI(){  
+    }
+	// Update is called once per frame
+	void Update() {
+    }
+
+    public IEnumerator DoCheck() 
+	{
+		while(true){
+			MicroPhoneInput.getInstance().StartRecord();
+			MicroPhoneInput.getInstance().save();
+			yield return new WaitForSeconds(10f);
+		}
+	}
+}

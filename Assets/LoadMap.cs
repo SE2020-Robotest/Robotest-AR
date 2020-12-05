069a0155d2784c5927ca45d1a0037cd63952a145
itoshiko <53360175+itoshiko@users.noreply.github.com>
2020-12-06 00:35:25 +0800

    loading map

---
+++ Assets/LoadMap.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CSharpGRPC.services;
+using Msg;
+
+
+namespace MapOperation
+{
+    public class LoadMap : MonoBehaviour
+    {
+        public void loadMap(Map request)
+        {
+            float roomwidth = (float)request.Roomwidth;
+            float roomheight = (float)request.Roomheight;
+            
+            GameObject exsistingMap = GameObject.Find("mainMap");
+            if (exsistingMap == null)
+            {
+                exsistingMap = new GameObject("mainMap");
+                exsistingMap.transform.parent = this.transform;
+            }
+            else
+            {
+                Destroy(exsistingMap);
+                exsistingMap = new GameObject("mainMap");
+                exsistingMap.transform.parent = this.transform;
+            }
+            List<GameObject> wallList = new List<GameObject>();
+            for (int i = 0; i < 4; i++)
+            {
+                wallList.Add(GameObject.CreatePrimitive(PrimitiveType.Plane));
+                wallList[i].transform.parent = this.transform;
+            }
+            wallList[0].transform.position = new Vector3((float)(roomwidth/20.0),5,0);
+            wallList[0].transform.localScale = new Vector3((float)(roomwidth/10.0),1,10);
+            wallList[0].transform.localEulerAngles = new Vector3(90,0,0);
+            
+            wallList[1].transform.position = new Vector3(0,5,(float)(roomheight/20.0));
+            wallList[1].transform.localScale = new Vector3((float)(roomheight/10.0),1,10);
+            wallList[1].transform.localEulerAngles = new Vector3(0,0,90);
+            
+            wallList[2].transform.position = new Vector3((float)(roomwidth/20.0),5,(float)(roomheight/10.0));
+            wallList[2].transform.localScale = new Vector3((float)(roomwidth/10.0),1,10);
+            wallList[2].transform.localEulerAngles = new Vector3(90,0,0);
+            
+            wallList[3].transform.position = new Vector3((float)(roomwidth/10.0),5,(float)(roomheight/20.0));
+            wallList[3].transform.localScale = new Vector3((float)(roomheight/10.0),1,10);
+            wallList[3].transform.localEulerAngles = new Vector3(0,0,90);
+            
+            Google.Protobuf.Collections.RepeatedField<Block> blocks = request.Blocks.Clone();
+            foreach (Block block in blocks)
+            {
+                Block.Types.Type type = block.Type;
+                if (type == Block.Types.Type.Cube)
+                {
+                    GameObject obstacles = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                    obstacles.transform.parent = this.transform;
+					obstacles.transform.localScale = new Vector3((float)(block.W/10.0),5,(float)(block.H/10.0));
+					//obstacles.transform.position = new Vector3((float)(block.X/10.0),2.5,(float)(block.Y/10.0));
+                }
+                if (type == Block.Types.Type.Cylinder)
+                {
+                    GameObject obstacles = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+                    obstacles.transform.parent = this.transform;
+					obstacles.transform.localScale = new Vector3((float)(block.W/10.0),5,(float)(block.H/10.0));
+					//obstacles.transform.position = new Vector3((float)(block.X/10.0),2.5,(float)(block.Y/10.0));
+                }
+            }
+        }
+    }
+}
+
---
+++ Assets/StartProtobuf.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CSharpGRPC.services;
+using System.Threading;
+
+public class StartProtobuf : MonoBehaviour
+{
+    // Start is called before the first frame update
+	//static Thread listenserverThread = new Thread(ARServer.StartServer);
+    void Start()
+    {
+		Thread listenserverThread = new Thread(ARServer.StartServer);
+        //ARServer.StartServer();
+		Debug.Log("Start Listen Server Thread");
+		listenserverThread.Start();
+		//Thread.Sleep(1000);
+		//ARServer.StopServer();
+		//Debug.Log("Stop AR Server");
+		//Thread.Sleep(1000);
+		//Debug.Log("Start AR Listen Server");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
